5f568e620b1d76e41daac5467106f4af729bfadc
joaquinns <joaquindevv96@gmail.com>
2022-04-23 12:15:31 -0400

    refactor to index page

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,12 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
 import { Layout } from "../components/Layout";
-import { ArticleCard } from "../components/ArticleCard";
 import { HOST_SV, PORT_SV } from "../config/config";
-import SearchBar from "components/SearchBar";
-import { SearchResults } from "components/SearchResults";
-
-const SEARCH_STATE = {
-  EMPTY: "",
-};
-
-const SEARCH_RESULT_STATE = {
-  EMPTY: [],
-};
+import { Home } from "components/Home";
 
 function HomePage({ articles }) {
-  const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
-
-  const handleChange = (e) => {
-    setSearch(e.target.value);
-  };
-
-  useEffect(() => {
-    async function searchedResults() {
-      const response = await axios.get(
-        HOST_SV + PORT_SV + `/api/articles/search/${search}`
-      );
-      //console.log(response.data);
-      setSearchResult(response.data);
-    }
-    searchedResults();
-  }, [search]);
-
   return (
     <Layout>
-      <SearchBar change={handleChange} />
-      <h1 className="text-center text-2xl font-bold">
-        {articles.length === 0 || searchResult.length === 0
-          ? ""
-          : `Resultado de "${search}"`}
-      </h1>
-      {searchResult === [] && <span>no existe</span>}
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
-        {searchResult.length > 0 ? (
-          <SearchResults searched={searchResult} />
-        ) : (
-          articles.map((article) => (
-            <ArticleCard key={article.articleid} article={article} />
-          ))
-        )}
-      </div>
+      <Home articles={articles} />
       {/*       <div>
         <h1 className="text-xl">Funciona! tailwind!</h1>
       </div>
