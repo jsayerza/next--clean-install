0515c86905427143c920fd8229b4b090fa7c6ea9
joaquinns <joaquindevv96@gmail.com>
2022-04-23 16:10:39 -0400

    search refined

--- components/Home.js
+++ components/Home.js
@@ -11,12 +11,13 @@ const SEARCH_STATE = {
 };
 
 const SEARCH_RESULT_STATE = {
+  NOT_FOUND: null,
   EMPTY: [],
 };
 
 export const Home = ({ articles }) => {
-  const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState([]);
+  const [search, setSearch] = useState(SEARCH_STATE.EMPTY);
+  const [searchResult, setSearchResult] = useState(SEARCH_RESULT_STATE.EMPTY);
 
   const handleChange = useCallback(
     debounce((e) => setSearch(e.target.value), 300),
@@ -28,33 +29,50 @@ export const Home = ({ articles }) => {
     const controller = new AbortController();
 
     async function searchedResults() {
-      const response = await axios.get(
-        HOST_SV + PORT_SV + `/api/articles/search/${search}`,
-        // señal para abortar la peticion
-        { signal: controller.signal }
-      );
-      //console.log(response.data);
-      setSearchResult(response.data);
+      if (search !== SEARCH_STATE.EMPTY) {
+        const response = await axios.get(
+          HOST_SV + PORT_SV + `/api/articles/search/${search}`,
+          // señal para abortar la peticion
+          { signal: controller.signal }
+        );
+        if (response.data.length === 0) {
+          return setSearchResult(null);
+        }
+        return setSearchResult(response.data);
+      }
+      return setSearchResult(articles);
     }
     searchedResults();
 
     // funcion que se dispara cuando se desmonta un componente de react
     return () => controller.abort();
-  }, [search]);
+  }, [articles, search]);
+
+  if (searchResult === null) {
+    return (
+      <>
+        <SearchBar change={handleChange} />
+        <h1 className="text-center text-2xl font-bold">
+          No se encontraron resultados de {`"${search}"`}
+        </h1>
+      </>
+    );
+  }
 
   return (
     <>
       <SearchBar change={handleChange} />
       <h1 className="text-center text-2xl font-bold">
-        {articles.length === 0 || searchResult.length === 0
+        {searchResult === SEARCH_RESULT_STATE.NOT_FOUND ||
+        search === SEARCH_STATE.EMPTY
           ? ""
           : `Resultado de "${search}"`}
       </h1>
-      {searchResult === [] && <span>no existe</span>}
       <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
-        {searchResult.length > 0 ? (
+        {searchResult?.length > 0 ? (
           <SearchResults searched={searchResult} />
         ) : (
+          search === SEARCH_STATE.EMPTY &&
           articles.map((article) => (
             <ArticleCard key={article.articleid} article={article} />
           ))
