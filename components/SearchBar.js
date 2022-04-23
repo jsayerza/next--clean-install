c5602e10c6b1df60903c11b7102303472d81dba8
joaquinns <joaquindevv96@gmail.com>
2022-04-23 12:14:53 -0400

    added style to input

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -2,7 +2,7 @@ export default function SearchBar({ change }) {
   return (
     <div>
       <input
-        className="p-2 text-md font-bold  w-1/2 my-2 mx-auto"
+        className="p-2 text-md font-bold  w-1/2 my-2 mx-auto shadow-sm"
         type="text"
         placeholder="Busca un articulo"
         onChange={change}
