1553117521c246482ca9572193e9c9114f0a9707
joaquinns <joaquindevv96@gmail.com>
2022-04-24 16:58:33 -0400

    created badge status

---
+++ components/BadgeStatus.js
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+
+export const BadgeStatus = ({ status }) => {
+  const classNamesDnD = clsx(
+    "rounded-full font-bold bg-green-600 px-3 py-1 text-white",
+    {
+      ["bg-green-400"]: status === "Bon estat",
+      ["bg-yellow-500"]: status === "Usat. Acceptable est",
+      ["bg-orange-500"]: status === "Deteriorat. Necessit",
+      ["bg-red-500"]: status === "No funciona. Per a p",
+    }
+  );
+
+  return <span className={classNamesDnD}>{status}</span>;
+};
