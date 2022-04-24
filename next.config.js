1dfa55029fdce48b8d73cc07bc5281a43e4bc6ba
joaquinns <joaquindevv96@gmail.com>
2022-04-24 15:43:35 -0400

    next config to img url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+};
