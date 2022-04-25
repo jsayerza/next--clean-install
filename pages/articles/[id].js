a4003373498fd0d9a5e16c61f27acccd9ac33bef
joaquinns <joaquindevv96@gmail.com>
2022-04-25 15:33:58 -0400

    fix of the buttons logic

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -6,10 +6,11 @@ import { HOST_SV, PORT_SV } from "config/config";
 import { Layout } from "../../components/Layout";
 import Image from "next/image";
 import { BadgeStatus } from "components/BadgeStatus";
+import { useUser } from "context/authContext";
 //import ButtonMailto from "components/ButtonMailTo";
 
 function ArticleView({ article }) {
-  //console.log("article: ", article);
+  const { user } = useUser();
 
   const router = useRouter();
 
@@ -64,12 +65,11 @@ function ArticleView({ article }) {
               <h2 className="text-lg text-gray-900 font-semibold pb-2">
                 Estat de l'article:
               </h2>
-{/*               <span className="rounded-full font-bold bg-green-600 px-3 py-1 text-white">
+              {/*               <span className="rounded-full font-bold bg-green-600 px-3 py-1 text-white">
                 {article.articlestatus}
               </span>
- */}              
-               <BadgeStatus status={article.articlestatus} />
-
+ */}
+              <BadgeStatus status={article.articlestatus} />
             </div>
 
             <div>
@@ -99,39 +99,42 @@ function ArticleView({ article }) {
               <h2 className="text-lg text-gray-900 font-bold pb-2">
                 {article.salestatus}
               </h2>
-              
             </div>
           </div>
 
-          <div className="my-12 flex justify-center">
-            <button
-              className="bg-cyan-600 hover:bg-gray-800 text-white text-lg font-bold rounded ml-2 py-3"
-              onClick={() => router.push(`mailto:${article.useremail}`)}
-            >
-              Contacta amb el venedor
-            </button>
-          </div>
+          {user.email !== article.useremail && (
+            <div className="my-12 flex justify-center">
+              <button
+                className="bg-cyan-600 hover:bg-gray-800 text-white text-lg font-bold rounded ml-2 py-3"
+                onClick={() => router.push(`mailto:${article.useremail}`)}
+              >
+                Contacta amb el venedor
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="py-5">
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white rounded px-3 py-2"
-          onClick={() => handleDelete(article.articleid)}
-        >
-          Elimina article
-        </button>
-        <button
-          className="bg-gray-500 hover:bg-gray-800 text-white rounded ml-2 py-2"
-          onClick={() => {
-            //console.log("ArticleView/article.articleid: ", article.articleid)
-            router.push("/articles/edit/" + article.articleid);
-          }}
-        >
-          Edita article
-        </button>
-        {/* <ButtonMailto label="Contacta amb el venedor" mailto="mailto: ${article.email}" /> */}
-      </div>
+      {user.email === article.useremail && (
+        <div className="py-5">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-white rounded px-3 py-2"
+            onClick={() => handleDelete(article.articleid)}
+          >
+            Elimina article
+          </button>
+          <button
+            className="bg-gray-500 hover:bg-gray-800 text-white rounded ml-2 px-3 py-2"
+            onClick={() => {
+              //console.log("ArticleView/article.articleid: ", article.articleid)
+              router.push("/articles/edit/" + article.articleid);
+            }}
+          >
+            Edita article
+          </button>
+          {/* <ButtonMailto label="Contacta amb el venedor" mailto="mailto: ${article.email}" /> */}
+        </div>
+      )}
     </Layout>
   );
 }
