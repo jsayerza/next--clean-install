0aeb6f5d570d26f45547370a113684fa63c47923
joaquinns <joaquindevv96@gmail.com>
2022-04-27 09:17:00 -0400

    style

--- pages/profile.js
+++ pages/profile.js
@@ -32,7 +32,7 @@ function ProfilePage() {
 
   return (
     <Layout>
-      <h1 className="text-left text-2xl font-bold my-2">Profile</h1>
+      <h1 className="text-left text-2xl font-bold my-2">Your Articles</h1>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
         {profileArticles.map((article) => (
           <ArticleCard key={article.articleid} article={article} />
