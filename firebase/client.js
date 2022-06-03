7283bc3d7612e369afb192eb819ba2f7df7ced9f
jsayerza <jsayerza.comptes@gmail.com>
2022-06-03 21:36:50 +0200

    clientFB

--- firebase/client.js
+++ firebase/client.js
@@ -17,6 +17,15 @@ import {
 
 
 const firebaseConfig = {
+  apiKey: "AIzaSyByQP6YvMi6uDvejkm93aRFGrC2sjXT430",
+  authDomain: "escolapop-pre.firebaseapp.com",
+  projectId: "escolapop-pre",
+  storageBucket: "escolapop-pre.appspot.com",
+  messagingSenderId: "669590894513",
+  appId: "1:669590894513:web:104db4bd09c13422b7af90",
+};
+
+/* const firebaseConfig = {
   apiKey: "AIzaSyDMrVCx3Czfj5Rcp52Dk544UKTaZ7CzRAg",
   authDomain: "escolapop-db7d2.firebaseapp.com",
   projectId: "escolapop-db7d2",
@@ -24,6 +33,7 @@ const firebaseConfig = {
   messagingSenderId: "22410271497",
   appId: "1:22410271497:web:a573e2f2e658e5aced91d8",
 };
+ */
 
 const app = initializeApp(firebaseConfig);
 
